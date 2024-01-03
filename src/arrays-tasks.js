b18f8441d68f317a49c03484a38cfbf3b32ae1fb
Alexey888888 <brdkmail@gmail.com>
2024-01-03 11:18:43 +0200

    feat: done propagateItemsByPositionIndex, shiftArray tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -567,8 +567,14 @@ function findLongestIncreasingSubsequence(nums) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const resArr = [];
+  arr.map((item, index) => {
+    resArr.length += index + 1;
+    resArr.fill(item, resArr.length - index - 1);
+    return '';
+  });
+  return resArr;
 }
 
 /**
@@ -584,8 +590,10 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  return n > 0
+    ? [...arr.slice(-n), ...arr.slice(0, arr.length - n)]
+    : [...arr.slice(-n, arr.length), ...arr.slice(0, -n)];
 }
 
 /**
